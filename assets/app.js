4a5e13f382214c5fa67e127f6cbd4561685bb73c
Mathieu54 <mathieuhrmt@gmail.com>
2022-10-20 13:12:39 +0200

    Add boostrap

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,5 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
-// start the Stimulus application
+import './styles/app.scss';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 import './bootstrap';
