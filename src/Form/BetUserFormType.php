3697b7c54f8098a5691400ff3e6abf3cffcb6813
Mathieu54 <mathieuhrmt@gmail.com>
2022-10-26 22:46:26 +0200

    Create form to edit or add bet user - Add form bet - Modified template twig - Delete temp file - Install twig error template (work in progress) - Add function BetController

--- src/Form/BetUserFormType.php
+++ src/Form/BetUserFormType.php
@@ -3,29 +3,29 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BetUserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        //TODO CHANGE HERE !!
         $builder
-            ->add('test', TextType::class, [
+            ->add('score_countrie_1', IntegerType::class, [
                 'required' => true,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Vous devez renseigner votre prénom',
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Votre prénom doit comporter au moins {{ limit }} caractères',
-                        'maxMessage' => 'Votre prénom ne doit pas dépasser {{ limit }} caractères',
-                        'max' => 100,
-                    ]),
+                        'message' => 'Vous ne pouvez pas envoyer un résultat vide',
+                    ])
+                ],
+            ])
+            ->add('score_countrie_2', IntegerType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous ne pouvez pas envoyer un résultat vide',
+                    ])
                 ],
             ]);
     }
