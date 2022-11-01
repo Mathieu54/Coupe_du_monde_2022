0c1e1f4fbf8085d6567558715aa083b4eca0d249
Mathieu54 <46712415+Mathieu54@users.noreply.github.com>
2022-11-01 15:50:45 +0100

    Add template error, change calculation scores - Add command calculation scores (work in progress) - Install mailhog and mailer with composer - Create template email (work in progress) - Change docker-composer

--- src/Command/CalculationScores.php
+++ src/Command/CalculationScores.php
@@ -9,16 +9,21 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 #[AsCommand(name: 'app:calculation_scores')]
 class CalculationScores extends Command
 {
     private $doctrine;
+    private $mailerInterface;
 
-    public function __construct(ManagerRegistry $doctrine)
+    public function __construct(ManagerRegistry $doctrine, MailerInterface $mailerInterface)
     {
         parent::__construct();
         $this->doctrine = $doctrine;
+        $this->mailerInterface = $mailerInterface;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,13 +9,16 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, MailerInterface $mailerInterface): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
