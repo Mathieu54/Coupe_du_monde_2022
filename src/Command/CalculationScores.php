90d22d741091dd7d0a95f800eb2253a4286824ba
Mathieu54 <46712415+Mathieu54@users.noreply.github.com>
2022-11-21 00:30:15 +0100

    Patch 2 and add information other user - Add information other participant if the user want to see the bet of other player - Click leadboard to see profil other user - Correction leadboard placement - Add log for the command calculation score - Add email start_bet (temp) - Add testemail2 (temp test in production)

--- src/Command/CalculationScores.php
+++ src/Command/CalculationScores.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Entity\BetUser;
 use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Psr\Log\LoggerInterface;
@@ -30,14 +31,20 @@ class CalculationScores extends Command
         $entityManager = $this->doctrine->getManager();
         $getBet = $this->doctrine->getRepository(BetUser::class)->findBy(['calculate' => '0']);
         $getDateNow = new DateTime();
+        $dateFormat = "d/m/Y G:i:s";
+        $output = "[%datetime%][%level_name%]: %message%\n";
+        $formatter = new LineFormatter($output, $dateFormat);
+        $stream = new StreamHandler('var/log/calculation_scores.log', Level::Debug);
+        $stream->setFormatter($formatter);
+        $log = new Logger('security');
+        $log->pushHandler($stream);
+        $log->info("=============================================");
+        $log->info("=============START CALCULATION===============");
+        $log->info("=============================================");
         foreach ($getBet as $bet) {
-            // create a log channel
-            $log = new Logger('name');
-            $log->pushHandler(new StreamHandler('var/log/calculation_scores.log', Level::Warning));
-
-            $log->warning('Foo');
-            $log->error('Bar');
             if (($getDateNow->getTimestamp() >= $bet->getMatches()->getDate()->getTimestamp()) && ($bet->getMatches()->getScoreCountrie1() !== null) && ($bet->getMatches()->getScoreCountrie2() !== null)) {
+                $log->info('Vérification Parie N°' . $bet->getId() . ' est calculé => ' . $bet->isCalculate());
+                $log->info($bet->getUser()->getName() . ' a parié => ' . $bet->getMatches()->getCountrie1()->getName() . ' ' . $bet->getScoreCountrie1() . ' - ' . $bet->getScoreCountrie2() . ' ' . $bet->getMatches()->getCountrie2()->getName());
                 $scores_final = $bet->getUser()->getUserScores()->getScores();
                 $bet_win_final = $bet->getUser()->getUserScores()->getBetWin();
                 $bet_win_bonus_final = $bet->getUser()->getUserScores()->getBetWinBonus();
@@ -45,28 +52,38 @@ class CalculationScores extends Command
                 if (($bet->getScoreCountrie1() == $bet->getMatches()->getScoreCountrie1()) && ($bet->getScoreCountrie2() == $bet->getMatches()->getScoreCountrie2())) {
                     $scores_final += 2;
                     $bet_win_bonus_final++;
+                    $log->info('+2 points car scores exactes');
                 }
                 if (($bet->getMatches()->getScoreCountrie1() == $bet->getMatches()->getScoreCountrie2()) && ($bet->getScoreCountrie1() == $bet->getScoreCountrie2())) {
                     $scores_final++;
                     $bet_win_final++;
+                    $log->info('+1 point équipe gagnante');
                 } else if (($bet->getMatches()->getScoreCountrie1() > $bet->getMatches()->getScoreCountrie2()) && ($bet->getScoreCountrie1() > $bet->getScoreCountrie2())) {
                     $scores_final++;
                     $bet_win_final++;
+                    $log->info('+1 point équipe gagnante');
                 } else if (($bet->getMatches()->getScoreCountrie1() < $bet->getMatches()->getScoreCountrie2()) && ($bet->getScoreCountrie1() < $bet->getScoreCountrie2())) {
                     $scores_final++;
                     $bet_win_final++;
+                    $log->info('+1 point équipe gagnante');
                 } else {
                     $bet_lose_final++;
+                    $log->info('0 point');
                 }
                 $bet->getUser()->getUserScores()->setBetLose($bet_lose_final);
                 $bet->getUser()->getUserScores()->setBetWinBonus($bet_win_bonus_final);
                 $bet->getUser()->getUserScores()->setBetWin($bet_win_final);
                 $bet->getUser()->getUserScores()->setScores($scores_final);
                 $bet->setCalculate(true);
+                $log->info('Parie N°' . $bet->getId() . ' est calculé => ' . $bet->isCalculate() . ' TERMINE !');
+                $log->info('=============================================================');
                 $entityManager->persist($bet);
                 $entityManager->flush();
             }
         }
+        $log->info("===========================================");
+        $log->info("=============END CALCULATION===============");
+        $log->info("===========================================");
         return Command::SUCCESS;
     }
 }
