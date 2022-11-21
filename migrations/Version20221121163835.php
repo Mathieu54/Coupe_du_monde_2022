26b14478bf557e5f077c2a34f74debea17fb1416
Mathieu54 <46712415+Mathieu54@users.noreply.github.com>
2022-11-21 17:37:49 +0100

    Add function remember me - Add remember me login page - Add table remember me - Correction text - Config change for remember me

---
+++ migrations/Version20221121163835.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221121163835 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE rememberme_token (series CHAR(88) PRIMARY KEY NOT NULL, value VARCHAR(88) NOT NULL, lastUsed datetime NOT NULL, class VARCHAR(100) NOT NULL, username VARCHAR(200) NOT NULL)');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE rememberme_token');
+    }
+}
