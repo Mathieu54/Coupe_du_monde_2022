0f77d9cd49ada4574198027604bbc743b02df00c
Mathieu54 <46712415+Mathieu54@users.noreply.github.com>
2022-11-22 13:16:13 +0100

    Create history bet - Create section history bet - Correction bug visuel scores

--- src/Controller/BetController.php
+++ src/Controller/BetController.php
@@ -29,20 +29,37 @@ class BetController extends AbstractController
         $list_matches_bet = [];
         foreach ($matches_type_7 as $matche) {
             $actual_bet_user = $doctrine->getRepository(BetUser::class)->findOneBy(["user" => $this->getUser()->getId(), "matches" => $matche->getId()]);
-            $list_matches_bet[$matche->getId()][] = [
-                "id" => $matche->getId(),
-                "countrie_1" => ($matche->getCountrie1() == null) ? "null" : $matche->getCountrie1()->getName(),
-                "countrie_1_flag" => ($matche->getCountrie1() == null) ? "null" : $matche->getCountrie1()->getIsoFlag(),
-                "countrie_2" => ($matche->getCountrie2() == null) ? "null" : $matche->getCountrie2()->getName(),
-                "countrie_2_flag" => ($matche->getCountrie2() == null) ? "null" : $matche->getCountrie2()->getIsoFlag(),
-                "date" => $matche->getDate(),
-                "date_2_hours" => (clone $matche->getDate())->add(new DateInterval("PT2H")),
-                "score_countrie_1" => $matche->getScoreCountrie1(),
-                "score_countrie_2" => $matche->getScoreCountrie2(),
-                "type_match" => $matche->getTypeMatch(),
-                "score_countrie_1_user" => ($actual_bet_user == null) ? "null" : $actual_bet_user->getScoreCountrie1(),
-                "score_countrie_2_user" => ($actual_bet_user == null) ? "null" : $actual_bet_user->getScoreCountrie2(),
-            ];
+            if(($matche->getDate()) > (new DateTime())) {
+                $list_matches_bet[$matche->getId()][] = [
+                    "id" => $matche->getId(),
+                    "countrie_1" => ($matche->getCountrie1() == null) ? "null" : $matche->getCountrie1()->getName(),
+                    "countrie_1_flag" => ($matche->getCountrie1() == null) ? "null" : $matche->getCountrie1()->getIsoFlag(),
+                    "countrie_2" => ($matche->getCountrie2() == null) ? "null" : $matche->getCountrie2()->getName(),
+                    "countrie_2_flag" => ($matche->getCountrie2() == null) ? "null" : $matche->getCountrie2()->getIsoFlag(),
+                    "date" => $matche->getDate(),
+                    "date_2_hours" => (clone $matche->getDate())->add(new DateInterval("PT2H")),
+                    "score_countrie_1" => $matche->getScoreCountrie1(),
+                    "score_countrie_2" => $matche->getScoreCountrie2(),
+                    "type_match" => $matche->getTypeMatch(),
+                    "score_countrie_1_user" => ($actual_bet_user == null) ? "null" : $actual_bet_user->getScoreCountrie1(),
+                    "score_countrie_2_user" => ($actual_bet_user == null) ? "null" : $actual_bet_user->getScoreCountrie2(),
+                ];
+            } else {
+                $list_matches_bet[$matche->getId()][] = [
+                    "id" => $matche->getId(),
+                    "countrie_1" => ($matche->getCountrie1() == null) ? "null" : $matche->getCountrie1()->getName(),
+                    "countrie_1_flag" => ($matche->getCountrie1() == null) ? "null" : $matche->getCountrie1()->getIsoFlag(),
+                    "countrie_2" => ($matche->getCountrie2() == null) ? "null" : $matche->getCountrie2()->getName(),
+                    "countrie_2_flag" => ($matche->getCountrie2() == null) ? "null" : $matche->getCountrie2()->getIsoFlag(),
+                    "date" => $matche->getDate(),
+                    "date_2_hours" => (clone $matche->getDate())->add(new DateInterval("PT2H")),
+                    "score_countrie_1" => $matche->getScoreCountrie1(),
+                    "score_countrie_2" => $matche->getScoreCountrie2(),
+                    "type_match" => 100,
+                    "score_countrie_1_user" => ($actual_bet_user == null) ? "null" : $actual_bet_user->getScoreCountrie1(),
+                    "score_countrie_2_user" => ($actual_bet_user == null) ? "null" : $actual_bet_user->getScoreCountrie2(),
+                ];
+            }
         }
         return $this->render('pages/bet.html.twig', [
             'matches' => $list_matches_bet,
